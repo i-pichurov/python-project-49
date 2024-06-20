33031c7580bffd964a30ef284da1b1a66d9b829d
i-pichurov <deadbassein@gmail.com>
2024-06-20 23:33:35 +0300

    add even_logic.py and brain_even.py

---
+++ brain_games/even_logic.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import prompt
+import random
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    return name
+
+
+def even_check(random_number):
+    if random_number % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def even_game():
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+    while i < 3:
+        random_number = random.randint(1, 100)
+        print(f'Question: {random_number}')
+        answer = prompt.string('Your answer: ')
+        if even_check(random_number) == answer:
+            print('Correct!')
+            i += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{even_check(random_number)}'.\nLet's try again, {name}!")
+            break
+    
+    if i == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import brain_games.even_logic
+
+
+def main():
+    brain_games.even_logic.even_game()
+
+
+if __name__ == '__main__':
+    main()
