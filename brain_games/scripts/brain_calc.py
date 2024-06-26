c4551d6448741bf1f92019a8eeb4cd60dda72983
i-pichurov <deadbassein@gmail.com>
2024-06-26 23:04:04 +0300

    fix brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,7 +4,7 @@ import brain_games.games.calc
 
 
 def main():
-    brain_games.games.engine.start_game(brain_games.games.calc.br_calc())
+    brain_games.games.engine.start_game(brain_games.games.calc.br_calc)
 
 
 if __name__ == '__main__':
