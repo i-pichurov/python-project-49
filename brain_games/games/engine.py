665eaa958843714dec05837410d62de71bdae53c
i-pichurov <deadbassein@gmail.com>
2024-07-15 23:59:45 +0300

    update calc, engine

--- brain_games/games/engine.py
+++ brain_games/games/engine.py
@@ -9,7 +9,7 @@ def start_game(game):
     i = 0
     while i < 3:
         (question, answer) = game()[1:3]
-        print(f'Question: {question}')
+        print(question)
         user_answer = prompt.string('Your answer: ')
         if answer == user_answer:
             print('Correct!')
