ca9a74dde6fe28bf28357d86f669b1e4e948ea3e
i-pichurov <deadbassein@gmail.com>
2024-07-16 22:56:17 +0300

    update prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,7 +5,7 @@ import random
 def br_prime():
     intro = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     num = random.randint(1, 3571)
-    question = f'{num}'
+    question = f'Question: {num}'
     if num > 1:
         answer = 'yes'
         for i in range(2, (num + 1)):
