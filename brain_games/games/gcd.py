e06427a98fac3c0ae324cb2f18eab20c24f475bc
i-pichurov <deadbassein@gmail.com>
2024-07-21 14:35:35 +0300

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,12 +1,7 @@
 import random
 
 
-def br_gcd():
-    intro = 'Find the greatest common divisor of given numbers.'
-    num1 = random.randint(1, 10000)
-    num2 = random.randint(1, 10000)
-    question = f'Question: {num1} {num2}'
-
+def gcd_calc(num1, num2):
     if num1 > num2:
         dividend = num1
         divisor = num2
@@ -18,4 +13,12 @@ def br_gcd():
         dividend = divisor
         divisor = i
         i = dividend % divisor
-    return (intro, question, str(divisor))
+    return divisor
+
+
+def br_gcd():
+    intro = 'Find the greatest common divisor of given numbers.'
+    num1 = random.randint(1, 10000)
+    num2 = random.randint(1, 10000)
+    question = f'Question: {num1} {num2}'
+    return (intro, question, str(gcd_calc(num1, num2)))
